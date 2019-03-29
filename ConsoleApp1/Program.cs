a924f5caa4c40a0bb740447aa7e65637601f5440
Broxam4 <48762143+Broxam4@users.noreply.github.com>
2019-03-29 21:34:35 +0100

    test22

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World2!");
+            Console.WriteLine("Hello World2!");
             Console.ReadKey();
         }
     }
