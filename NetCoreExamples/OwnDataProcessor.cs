88380b440d7d468470468457cbcd5ca77ab7f244
Broxam4 <48762143+Broxam4@users.noreply.github.com>
2019-04-03 17:26:49 +0200

    Upload as of 3.4.19

--- NetCoreExamples/OwnDataProcessor.cs
+++ NetCoreExamples/OwnDataProcessor.cs
@@ -1,4 +1,5 @@
 ﻿using MbientLab.MetaWear;
+using MbientLab.MetaWear.Core;
 using MbientLab.MetaWear.Data;
 using MbientLab.MetaWear.Sensor;
 using MbientLab.MetaWear.Sensor.GyroBmi160;
@@ -24,37 +25,44 @@ namespace NetCoreExamples
 
             acc.Configure(odr: 100f);
             gyro.Configure(odr: OutputDataRate._100Hz);
-            //AngularVelocity array1;
-            double x1 = 0;
-            int i = 1;
+            AngularVelocity array1;
+            float xgy = 0, ygy = 0, zgy = 0;
+            //int i = 1;
             int j = 1;
             await gyro.AngularVelocity.AddRouteAsync(source => source.Stream(data => 
             {
                 array1 = data.Value<AngularVelocity>();
-                Console.WriteLine(i++ +": " + array1);
-                x1 = array1.X;
-
+                //Console.WriteLine(i++ +": " + array1);
+                xgy += array1.X;
+                ygy += array1.Y;
+                zgy += array1.Z;
             }));
             Acceleration array2;
+            float xacc = 0, yacc = 0, zacc = 0;
             await acc.Acceleration.AddRouteAsync(source => source.Stream(data2 =>
             {
                 array2 = data2.Value<Acceleration>();
-                Console.WriteLine(j++ + ": " + array2);
+                xacc = array2.X;
+                yacc = array2.Y;
+                zacc = array2.Z;
+                //Console.WriteLine(j++ + ": " + array2);
             }));
             //Console.WriteLine(array1.ToString());
-            Console.WriteLine("test1");
             gyro.AngularVelocity.Start();
             acc.Acceleration.Start();
-            Console.WriteLine("test2");
             gyro.Start();
             acc.Start();
             //TODO Synchronise gyro and acc to get matching data
-            await Task.Delay(15000);
-
+            for (int i = 0; i < 1000; i++)
+            {
+                await Task.Delay(50);
+                Console.WriteLine(String.Format("{0} {1} {2}:{3} {4} {5}", xgy, ygy, zgy, xacc, yacc, zacc));
+                xgy = ygy = zgy = 0;
+            }
             gyro.Stop();
             acc.Stop();
             Console.WriteLine();
-            Console.WriteLine(x1);
+            //Console.WriteLine(x1);
             Console.ReadLine();
         }
     }
