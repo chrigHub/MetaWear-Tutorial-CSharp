2759da77188fa68b342b050be1cb014742f6ad53
Broxam4 <48762143+Broxam4@users.noreply.github.com>
2019-05-11 11:49:02 +0200

    Update 11.5.19

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,10 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World2!");
-            Console.WriteLine("Hello World3!");
-            Console.ReadKey();
+            double x = 2
         }
     }
 }
